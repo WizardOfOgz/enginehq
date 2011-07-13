0950bef36e1c44d4f24553a623affa8c6fbe4d31
Kyle J. Ginavan <kylejginavan@gmail.com>
2011-07-12 19:17:30 -0500

    layout files for hq

--- lib/generatorhq.rb
+++ lib/generatorhq.rb
@@ -7,9 +7,9 @@ module Generatorhq
       config.mount_at = '/'
 
       # Load rake tasks
-      rake_tasks do
-        load File.join(File.dirname(__FILE__), 'rails/railties/tasks.rake')
-      end
+      # rake_tasks do
+      #   load File.join(File.dirname(__FILE__), 'rails/railties/tasks.rake')
+      # end
 
       # Check the gem config
       initializer "check config" do |app|
