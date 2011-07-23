476fcd09cd8db74f08669628d2ad6878f2067361
Kyle J. Ginavan <kylejginavan@gmail.com>
2011-07-23 10:26:05 -0500

    just got it working

--- lib/enginehq.rb
+++ lib/enginehq.rb
@@ -1,25 +1,36 @@
 require "enginehq/version"
-
 module Enginehq
   class Engine < Rails::Engine
-      # Config defaults
-      config.widget_factory_name = "default factory name"
-      config.mount_at = '/'
-
-      # Load rake tasks
-      # rake_tasks do
-      #   load File.join(File.dirname(__FILE__), 'rails/railties/tasks.rake')
-      # end
-
-      # Check the gem config
-      initializer "check config" do |app|
-
-        # make sure mount_at ends with trailing slash
-        config.mount_at += '/'  unless config.mount_at.last == '/'
-      end
-
-      initializer "static assets" do |app|
-        app.middleware.use ::ActionDispatch::Static, "#{root}/public"
-      end
+    #engine_name :enginehq
+    # Config defaults
+    config.widget_factory_name = "default factory name"
+    config.mount_at = '/'
+    # Check the gem config
+    initializer "check config" do |app|
+      # make sure mount_at ends with trailing slash
+      config.mount_at += '/'  unless config.mount_at.last == '/'
     end
+
+    #TODO: load all and not define individually
+    rake_tasks do
+      load "#{root}/lib/tasks/clear.rake"
+      load "#{root}/lib/tasks/palette.rake"
+      load "#{root}/lib/tasks/start.rake"
+    end
+
+    # include in application controller
+    # initializer 'enginehq.app_controller' do |app|
+    #   ActiveSupport.on_load(:action_controller) do
+    #     include MyModule::InstanceMethods
+    #   end
+    # end
+
+    initializer "static assets" do |app|
+      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+    end
+
+    config.to_prepare do
+      ApplicationController.helper(EnginehqHelper)
+    end
+  end
 end
