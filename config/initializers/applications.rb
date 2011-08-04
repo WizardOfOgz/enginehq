4c2541e638608dd377fef39c49fabbb01aac3718
Kyle J. Ginavan <kylejginavan@gmail.com>
2011-08-04 15:13:11 -0500

    removed https from urls

--- config/initializers/applications.rb
+++ config/initializers/applications.rb
@@ -2,7 +2,7 @@ module Applications
   AGENCIESHQ = {:url => Rails.env.production? ? "https://agencieshq.com" : "localhost:3000",
     :name => "AgenciesHQ", :tabs => ["Advisors", "Policies", "Contracts", "Licenses"],
     :more => ["Agencies", "Carriers", "Products", "Policy Holders", "Transfer Co.", "Programs", "Premium Reports", "Activity Reports"]}
-  
+
   MARKETINGHQ = {:url => Rails.env.production? ? "http://marketinghq.agencieshq.com" : "localhost:3001",
     :name => "MarketingHQ", :tabs => ["Campaigns", "Lists", "Newsletters", "Templates"],
     :more => []}
@@ -14,9 +14,9 @@ module Applications
   REPORTINGHQ = {:url => Rails.env.production? ? "http://reportinghq.agencieshq.com" : "localhost:3003",
     :name => "ReportingingHQ", :tabs => [""],
     :more => []}
-  
+
   APPLICATIONS = [AGENCIESHQ, MARKETINGHQ, ADMINISTRATORSHQ, REPORTINGHQ]
-  
+
   def self.get_application_option(option)
     eval("Applications::#{Rails.application.class.parent_name.upcase}[:#{option}]")
   end
