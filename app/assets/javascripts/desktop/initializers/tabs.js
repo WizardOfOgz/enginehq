d484dc55acfe110aa78e00b477e77829fb987994
Michael Hemesath <mike.hemesath@gmail.com>
2011-08-24 13:31:18 -0500

    removed gradients, updated tabs initializer

--- app/assets/javascripts/desktop/initializers/tabs.js
+++ app/assets/javascripts/desktop/initializers/tabs.js
@@ -5,15 +5,18 @@
   
   
   function tab(e) { 
-    var group = $(this).closest("ul"),
-        selectedTab = $(this).parent().addClass("selected"),
-        tabSiblings = selectedTab.siblings().removeClass("selected");
+    var group = $(this).closest("ul")
+      , selectedTab = $(this).parent().addClass("selected")
+      , tabSiblings = selectedTab.siblings().removeClass("selected");
 
   	tabSiblings.children("a").each(function() { $(this.hash).hide(); });
-  	$(this.hash).show();
+  	var container = $(this.hash).show();
   	
   	AdvisorsHQ.setSelectedTab(group.get(0).className, this.hash);
 
+    if (container.length < 1) {
+      return;
+    }
   	e.stopPropagation();
   	e.preventDefault();
   }
