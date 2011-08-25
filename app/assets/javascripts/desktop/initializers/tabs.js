f63bf903f0b2e4b5fd5a962fb1693ec6a3cd0fcc
Michael Hemesath <mike.hemesath@gmail.com>
2011-08-25 16:33:39 -0500

    updated tabs

--- app/assets/javascripts/desktop/initializers/tabs.js
+++ app/assets/javascripts/desktop/initializers/tabs.js
@@ -1,30 +1,24 @@
-(function() {
-  $(document).delegate(".content-links a", "click", tab);
-  $(document).delegate(".field-group-index a", "click", tab);
-  $(document).delegate(".field-group-navigation a", "click", tab);
-  
-  
-  function tab(e) { 
-    var group = $(this).closest("ul")
-      , selectedTab = $(this).parent().addClass("selected")
-      , tabSiblings = selectedTab.siblings().removeClass("selected");
-
-  	tabSiblings.children("a").each(function() { $(this.hash).hide(); });
-  	var container = $(this.hash).show();
-  	
-  	AdvisorsHQ.setSelectedTab(group.get(0).className, this.hash);
-
-    if (container.length < 1) {
-      container = $("<div></div>").attr("id", this.hash.substr(1)).insertAfter(group).show();
-      AdvisorsHQ.load(this.href);
-      /*$.get(this.href, function(html) {
-        html = $(html);
-        container.replaceWith(html);
-        html.show();
-      });*/
-    } 
-    
-    e.stopPropagation();
-    e.preventDefault();
+$(document).delegate("a", "click", function tab(e) { 
+  if (this.hash.length < 1) {
+    return;
   }
-}());
+  
+  var group = $(this).closest("ul")
+    , selectedTab = $(this).parent().addClass("selected")
+    , tabSiblings = selectedTab.siblings().removeClass("selected");
+ 
+  tabSiblings.children("a").each(function() { $(this.hash).hide(); });
+  var container = $(this.hash).show();
+  
+  AdvisorsHQ.setSelectedTab(group.get(0).className, this.hash);
+ 
+  if (container.length < 1) {
+    container = $("<div></div>").attr("id", this.hash.substr(1)).css({display: "block"});
+    $("#contentSections").append(container);
+    
+    AdvisorsHQ.load(this.href);
+  } 
+  
+  e.stopPropagation();
+  e.preventDefault();
+});
