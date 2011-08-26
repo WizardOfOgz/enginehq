075df7ec97ee0768f4c892e3407bceebdec1cfed
Michael Hemesath <mike.hemesath@gmail.com>
2011-08-26 08:41:59 -0500

    updated tabs

--- app/assets/javascripts/desktop/initializers/tabs.js
+++ app/assets/javascripts/desktop/initializers/tabs.js
@@ -4,20 +4,37 @@ $(document).delegate("a", "click", function tab(e) {
   }
   
   var group = $(this).closest("ul")
-    , selectedTab = $(this).parent().addClass("selected")
-    , tabSiblings = selectedTab.siblings().removeClass("selected");
- 
-  tabSiblings.children("a").each(function() { $(this.hash).hide(); });
-  var container = $(this.hash).show();
+    , container = $(this.hash);
   
-  AdvisorsHQ.setSelectedTab(group.get(0).className, this.hash);
- 
-  if (container.length < 1) {
+  // if the container doesn't exist or its a content section, set main tabs
+  if (container.length < 1 || container.hasClass("content-section")) {
+    group = $("#contentLinks");
+    group.children().removeClass("selected").children("a[href$=" + this.hash + "]").parent().addClass("selected");
+    
+    // Links that are not in the tab menu ALWAYS trigger AJAX calls
+    if (!jQuery.contains(group.get(0), this)) {
+      container.remove();
+      container = null;
+    }
+  } else {
+    $(this).parent().addClass("selected").siblings().removeClass("selected")
+  }
+
+  if (!container || container.length < 1) {
     container = $("<div></div>").attr("id", this.hash.substr(1)).css({display: "block"});
     $("#contentSections").append(container);
-    
     AdvisorsHQ.load(this.href);
   } 
+  
+  group.find("a").each(function() { 
+    $(this.hash).hide(); 
+  });
+  
+  container.show();
+  
+  // save tab selection
+  AdvisorsHQ.setSelectedTab(group.get(0).className, this.hash);
+  
   
   e.stopPropagation();
   e.preventDefault();
