3c4318a365c6377aa00581adcaf413cddbe61fcf
Michael Hemesath <mike.hemesath@gmail.com>
2011-08-31 12:35:22 -0500

    added spinner script

--- app/assets/javascripts/desktop/components/loading.js
+++ app/assets/javascripts/desktop/components/loading.js
@@ -9,8 +9,22 @@ $(function() {
       clear();
     }
     
-    indicator = $("<p class=\"loading-indicator\"><span>" + message + "</span></p>");
+    indicator = $("<div class=\"loading-indicator\"><div>" + message + "</div></div>");
     indicator.appendTo(document.body);
+    var spinner = new Spinner({
+      lines: 10, // The number of lines to draw
+      length: 3, // The length of each line
+      width: 2, // The line thickness
+      radius: 3, // The radius of the inner circle
+      color: '#16355B', // #rbg or #rrggbb
+      speed: 2, // Rounds per second
+      trail: 100, // Afterglow percentage
+      shadow: false // Whether to render a shadow
+    }).spin();
+    
+    $(spinner.el).css({ position: "absolute", left: "12px", top: "50%", marginTop: "-1px" })
+    indicator.children().append(spinner.el);
+    
   });
   
   $(document).bind("xhrEnd", clear);
