0f1695caceb98107472ffe12e53524344e3235d8
Kyle J. Ginavan <kylejginavan@gmail.com>
2011-09-07 18:49:53 -0500

    unminified spinjs

--- app/assets/javascripts/desktop/lib/spin.js
+++ app/assets/javascripts/desktop/lib/spin.js
@@ -1,2 +1,278 @@
-//fgnass.github.com/spin.js
-(function(a,b,c){function O(a){H(arguments,function(b,d){a[b]===c&&(a[b]=d)});return a}function N(a){H(arguments,function(b,c){a[m][M(a,b)||b]=c});return a}function M(a,b){var d=a[m],f,g;if(d[b]!==c)return b;b=b.charAt(0).toUpperCase()+b.slice(1);for(g=0;g<E[e];g++){f=E[g]+b;if(d[f]!==c)return f}}function L(a,b){var c=[j,b,~~(a*100)].join("-"),d="{"+j+":"+a+"}",f;if(!F[c]){for(f=0;f<E[e];f++)try{K.insertRule("@"+(E[f]&&"-"+E[f].toLowerCase()+"-"||"")+"keyframes "+c+"{0%{"+j+":1}"+b+"%"+d+"to"+d+"}",K.cssRules[e])}catch(g){}F[c]=1}return c}function J(a,b,c){c&&!c[t]&&J(a,c),a.insertBefore(b,c||null);return a}function I(a){var c=b.createElement(a||"div");H(arguments,function(a,b){c[a]=b});return c}function H(a,b){var c=a[e]-1>>1;for(var d=1;d<=c;d++)b(a[d*2-1],a[d*2])}var d="width",e="length",f="radius",g="lines",h="trail",i="color",j="opacity",k="speed",l="shadow",m="style",n="height",o="left",p="top",q="px",r="childNodes",s="firstChild",t="parentNode",u="position",v="relative",w="absolute",x="animation",y="transform",z="Origin",A="Timeout",B="coord",C="#000",D=m+"Sheets",E="webkit0Moz0ms0O".split(0),F={},G;J(b.getElementsByTagName("head")[0],I(m));var K=b[D][b[D][e]-1],P=function(a){this.opts=O(a||{},g,12,h,100,e,7,d,5,f,10,i,C,j,.25,k,1)},Q=P.prototype={spin:function(b){var c=this,d=c.el=c[g](c.opts);b&&J(b,N(d,o,(b.offsetWidth>>1)+q,p,(b.offsetHeight>>1)+q),b[s]);if(!G){var e=c.opts,f=0,i=20/e[k],l=(1-e[j])/(i*e[h]/100),m=i/e[g];(function n(){f++;for(var b=e[g];b;b--){var h=Math.max(1-(f+b*m)%i*l,e[j]);c[j](d,e[g]-b,h,e)}c[A]=c.el&&a["set"+A](n,50)})()}return c},stop:function(){var b=this,d=b.el;a["clear"+A](b[A]),d&&d[t]&&d[t].removeChild(d),b.el=c;return b}};Q[g]=function(a){function s(b,c){return N(I(),u,w,d,a[e]+a[d]+q,n,a[d]+q,"background",b,"boxShadow",c,y+z,o,y,"rotate("+~~(360/a[g]*m)+"deg) translate("+a[f]+q+",0)","borderRadius","100em")}var b=N(I(),u,v),c=L(a[j],a[h]),m=0,r;for(;m<a[g];m++)r=N(I(),u,w,p,1+~(a[d]/2)+q,y,"translate3d(0,0,0)",j,a[j],x,c+" "+1/a[k]+"s linear infinite "+1/a[g]/a[k]*m+"s"),a[l]&&J(r,N(s(C,"0 0 4px "+C),p,2+q)),J(b,J(r,s(a[i],"0 0 1px rgba(0,0,0,.1)")));return b},Q[j]=function(a,b,c){a[r][b][m][j]=c};var R="behavior",S="url(#default#VML)",T="group0roundrect0fill0stroke".split(0);(function(){var a=N(I(T[0]),R,S),b;if(!M(a,y)&&a.adj){for(b=0;b<T[e];b++)K.addRule(T[b],R+":"+S);Q[g]=function(){function s(c,e,l){J(k,J(N(h(),"rotation",360/a[g]*c+"deg",o,~~e),J(N(I(T[1],"arcsize",1),d,b,n,a[d],o,a[f],p,-a[d]>>1,"filter",l),I(T[2],i,a[i],j,a[j]),I(T[3],j,0))))}function h(){return N(I(T[0],B+"size",c+" "+c,B+z,-b+" "+ -b),d,c,n,c)}var a=this.opts,b=a[e]+a[d],c=2*b,k=h(),m=~(a[e]+a[f]+a[d])+q,r;if(a[l])for(r=1;r<=a[g];r++)s(r,-2,"progid:DXImage"+y+".Microsoft.Blur(pixel"+f+"=2,make"+l+"=1,"+l+j+"=.3)");for(r=1;r<=a[g];r++)s(r);return J(N(I(),"margin",m+" 0 0 "+m,u,v),k)},Q[j]=function(a,b,c,d){d=d[l]&&d[g]||0,a[s][r][b+d][s][s][j]=c}}else G=M(a,x)})(),a.Spinner=P})(window,document)
+//fgnass.github.com/spin.js#v1.1
+(function(window, document, undefined) {
+
+/**
+ * Copyright (c) 2011 Felix Gnass [fgnass at neteye dot de]
+ * Licensed under the MIT license
+ */
+
+  var prefixes = ['webkit', 'Moz', 'ms', 'O'], /* Vendor prefixes */
+      animations = {}, /* Animation rules keyed by their name */
+      useCssAnimations;
+
+  /**
+   * Utility function to create elements. If no tag name is given,
+   * a DIV is created. Optionally properties can be passed.
+   */
+  function createEl(tag, prop) {
+    var el = document.createElement(tag || 'div'),
+        n;
+
+    for(n in prop) {
+      el[n] = prop[n];
+    }
+    return el;
+  }
+
+  /**
+   * Inserts child1 before child2. If child2 is not specified,
+   * child1 is appended. If child2 has no parentNode, child2 is
+   * appended first.
+   */
+  function ins(parent, child1, child2) {
+    if(child2 && !child2.parentNode) ins(parent, child2);
+    parent.insertBefore(child1, child2||null);
+    return parent;
+  }
+
+  /**
+   * Insert a new stylesheet to hold the @keyframe or VML rules.
+   */
+  ins(document.getElementsByTagName('head')[0], createEl('style'));
+  var sheet = document.styleSheets[document.styleSheets.length-1];
+
+  /**
+   * Creates an opacity keyframe animation rule and returns its name.
+   * Since most mobile Webkits have timing issues with animation-delay,
+   * we create separate rules for each line/segment.
+   */
+  function addAnimation(alpha, trail, i, lines) {
+    var name = ['opacity', trail, ~~(alpha*100), i, lines].join('-'),
+        start = 0.01 + i/lines*100,
+        z = Math.max(1-(1-alpha)/trail*(100-start) , alpha),
+        prefix = useCssAnimations.substring(0, useCssAnimations.indexOf('Animation')).toLowerCase(),
+        pre = prefix && '-'+prefix+'-' || '';
+
+    if (!animations[name]) {
+      sheet.insertRule(
+        '@' + pre + 'keyframes ' + name + '{' +
+        '0%{opacity:'+z+'}' +
+        start + '%{opacity:'+ alpha + '}' +
+        (start+0.01) + '%{opacity:1}' +
+        (start+trail)%100 + '%{opacity:'+ alpha + '}' +
+        '100%{opacity:'+ z + '}' +
+        '}', 0);
+      animations[name] = 1;
+    }
+    return name;
+  }
+
+  /**
+   * Tries various vendor prefixes and returns the first supported property.
+   **/
+  function vendor(el, prop) {
+    var s = el.style,
+        pp,
+        i;
+
+    if(s[prop] !== undefined) return prop;
+    prop = prop.charAt(0).toUpperCase() + prop.slice(1);
+    for(i=0; i<prefixes.length; i++) {
+      pp = prefixes[i]+prop;
+      if(s[pp] !== undefined) return pp;
+    }
+  }
+
+  /**
+   * Sets multiple style properties at once.
+   */
+  function css(el, prop) {
+    for (var n in prop) {
+      el.style[vendor(el, n)||n] = prop[n];
+    }
+    return el;
+  }
+
+  /**
+   * Fills in default values.
+   */
+  function defaults(obj, def) {
+    for (var n in def) {
+      if (obj[n] === undefined) obj[n] = def[n];
+    }
+    return obj;
+  }
+
+  /**
+   * Returns the absolute page-offset of the given element.
+   */
+  function pos(el) {
+    var o = {x:el.offsetLeft, y:el.offsetTop};
+    while((el = el.offsetParent)) {
+      o.x+=el.offsetLeft;
+      o.y+=el.offsetTop;
+    }
+    return o;
+  }
+
+  /** The constructor */
+  var Spinner = function Spinner(o) {
+    this.opts = defaults(o || {}, {
+      lines: 12, // The number of lines to draw
+      length: 7, // The length of each line
+      width: 5, // The line thickness
+      radius: 10, // The radius of the inner circle
+      color: '#000', // #rbg or #rrggbb
+      speed: 1, // Rounds per second
+      trail: 100, // Afterglow percentage
+      opacity: 1/4
+    });
+  },
+  proto = Spinner.prototype = {
+    spin: function(target) {
+      var self = this,
+          el = self.el = css(createEl(), {position: 'relative'}),
+          ep, // element position
+          tp; // target position
+
+      if (target) {
+        tp = pos(ins(target, el, target.firstChild));
+        ep = pos(el);
+        css(el, {
+          left: (target.offsetWidth >> 1) - ep.x+tp.x + 'px',
+          top: (target.offsetHeight >> 1) - ep.y+tp.y + 'px'
+        });
+      }
+      self.lines(el, self.opts);
+      if (!useCssAnimations) {
+        // No CSS animation support, use setTimeout() instead
+        var o = self.opts,
+            i = 0,
+            f = 20/o.speed,
+            ostep = (1-o.opacity)/(f*o.trail / 100),
+            astep = f/o.lines;
+
+        (function anim() {
+          i++;
+          for (var s=o.lines; s; s--) {
+            var alpha = Math.max(1-(i+s*astep)%f * ostep, o.opacity);
+            self.opacity(el, o.lines-s, alpha, o);
+          }
+          self.timeout = self.el && setTimeout(anim, 50);
+        })();
+      }
+      return self;
+    },
+    stop: function() {
+      var self = this,
+          el = self.el;
+
+      clearTimeout(self.timeout);
+      if (el && el.parentNode) el.parentNode.removeChild(el);
+      self.el = undefined;
+      return self;
+    }
+  };
+  proto.lines = function(el, o) {
+    var i = 0,
+        seg;
+
+    function fill(color, shadow) {
+      return css(createEl(), {
+        position: 'absolute',
+        width: (o.length+o.width) + 'px',
+        height: o.width + 'px',
+        background: color,
+        boxShadow: shadow,
+        transformOrigin: 'left',
+        transform: 'rotate(' + ~~(360/o.lines*i) + 'deg) translate(' + o.radius+'px' +',0)',
+        borderRadius: (o.width>>1) + 'px'
+      });
+    }
+    for (; i < o.lines; i++) {
+      seg = css(createEl(), {
+        position: 'absolute',
+        top: 1+~(o.width/2) + 'px',
+        transform: 'translate3d(0,0,0)',
+        opacity: o.opacity,
+        animation: useCssAnimations && addAnimation(o.opacity, o.trail, i, o.lines) + ' ' + 1/o.speed + 's linear infinite'
+      });
+      if (o.shadow) ins(seg, css(fill('#000', '0 0 4px ' + '#000'), {top: 2+'px'}));
+      ins(el, ins(seg, fill(o.color, '0 0 1px rgba(0,0,0,.1)')));
+    }
+    return el;
+  };
+  proto.opacity = function(el, i, val) {
+    el.childNodes[i].style.opacity = val;
+  };
+
+  /////////////////////////////////////////////////////////////////////////
+  // VML rendering for IE
+  /////////////////////////////////////////////////////////////////////////
+
+  /** 
+   * Check and init VML support
+   */
+  (function() {
+    var s = css(createEl('group'), {behavior: 'url(#default#VML)'}),
+        i;
+
+    if (!vendor(s, 'transform') && s.adj) {
+
+      // VML support detected. Insert CSS rules ...
+      for (i=4; i--;) sheet.addRule(['group', 'roundrect', 'fill', 'stroke'][i], 'behavior:url(#default#VML)');
+
+      proto.lines = function(el, o) {
+        var r = o.length+o.width,
+            s = 2*r;
+
+        function grp() {
+          return css(createEl('group', {coordsize: s +' '+s, coordorigin: -r +' '+-r}), {width: s, height: s});
+        }
+
+        var g = grp(),
+            margin = ~(o.length+o.radius+o.width)+'px',
+            i;
+
+        function seg(i, dx, filter) {
+          ins(g,
+            ins(css(grp(), {rotation: 360 / o.lines * i + 'deg', left: ~~dx}),
+              ins(css(createEl('roundrect', {arcsize: 1}), {
+                  width: r,
+                  height: o.width,
+                  left: o.radius,
+                  top: -o.width>>1,
+                  filter: filter
+                }),
+                createEl('fill', {color: o.color, opacity: o.opacity}),
+                createEl('stroke', {opacity: 0}) // transparent stroke to fix color bleeding upon opacity change
+              )
+            )
+          );
+        }
+
+        if (o.shadow) {
+          for (i = 1; i <= o.lines; i++) {
+            seg(i, -2, 'progid:DXImageTransform.Microsoft.Blur(pixelradius=2,makeshadow=1,shadowopacity=.3)');
+          }
+        }
+        for (i = 1; i <= o.lines; i++) {
+          seg(i);
+        }
+        return ins(css(el, {
+          margin: margin + ' 0 0 ' + margin
+        }), g);
+      };
+      proto.opacity = function(el, i, val, o) {
+        o = o.shadow && o.lines || 0;
+        el.firstChild.childNodes[i+o].firstChild.firstChild.opacity = val;
+      };
+    }
+    else {
+      useCssAnimations = vendor(s, 'animation');
+    }
+  })();
+
+  window.Spinner = Spinner;
+
+})(window, document);
