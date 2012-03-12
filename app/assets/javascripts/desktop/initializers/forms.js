efe2f08832b586f24af09c9ea031cb796ece7195
Kyle J. Ginavan <kylejginavan@gmail.com>
2012-03-12 12:54:16 -0500

    remove return false and add prevent Default on remove/add

--- app/assets/javascripts/desktop/initializers/forms.js
+++ app/assets/javascripts/desktop/initializers/forms.js
@@ -5,3 +5,12 @@ $(document).delegate(".field-group-head input[name='add'][type='submit'], input[
   HQApp.load(form.attr("action"), $.param(form.serializeArray().concat([{"name": submit.attr("name"), "value":submit.val()}])), {method:"post"}, submit);
   e.preventDefault();
 });
+
+var formSubmitted = false;
+$(document).on('submit', '.record-form', function(e) {
+  if(formSubmitted) {
+    e.preventDefault();
+  } else {
+    formSubmitted = true;
+  }
+});
