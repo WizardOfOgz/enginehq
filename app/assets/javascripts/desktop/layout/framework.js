31144a93d1f3d95abc92c3a22e90f06ab8dbb8bd
Kyle J. Ginavan <kylejginavan@gmail.com>
2012-03-26 09:43:47 -0500

    fix for messages

--- app/assets/javascripts/desktop/layout/framework.js
+++ app/assets/javascripts/desktop/layout/framework.js
@@ -1,15 +1,3 @@
-$(document).on('focus', '.message-form .note textarea', function() {
-  var textarea = $(this);
-  $(document).one('click', 'a, input', function(e) {
-    if(textarea.val() !== '' && !$(this).hasClass('save-action')) {
-      if(!confirm('You have not submitted your note. Would you like to leave this page?')) {
-        e.preventDefault(); // don't follow the link
-        e.stopPropagation(); // don't allow other handlers to continue with ajax
-      }
-    }
-  });
-});
-
 (function() {
   
   var xhrs = [],
