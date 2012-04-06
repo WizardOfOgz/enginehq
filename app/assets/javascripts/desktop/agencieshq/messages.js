b725ccdbda91b904189082d71755762937ee4649
Kyle J. Ginavan <kylejginavan@gmail.com>
2012-04-06 10:00:26 -0500

    message js prod error

--- app/assets/javascripts/desktop/agencieshq/messages.js
+++ app/assets/javascripts/desktop/agencieshq/messages.js
@@ -28,71 +28,69 @@ $(document).on('focus', '.message-form .note textarea, .message-form .subject in
   var textarea = $(this); // or input subject
   var tab = $('.content-links').find('.selected');
 
-  // make sure none of the inputs are those in the note form
+  // make sure none of the inputs are those in the message form
   var formElements = $(this).parents('form').find('input');
-  var inputElement = $(this).parents('form').find('input[type=submit]');
+  var submitElement = $(this).parents('form').find('input[type=submit]');
   var bindings = $('a, input').not(formElements);
 
   bindings.bind('click.preventNavigation', function(e) {
 
     if(textarea.val() !== '') {
-  
+
       if(e.isDefaultPrevented()) { 
       // if there's already an event handler on the element that is preventing the default action
-        noteConfirmation(textarea, tab, bindings, false);
+        messageConfirmation(textarea, tab, bindings, false);
       } else { 
         // if there isn't an event handler, we want to allow them to continue with the action, so we pass it along
         e.preventDefault();
-        noteConfirmation(textarea, tab, bindings, $(this));
+        messageConfirmation(textarea, tab, bindings, $(this));
       }
 
     } else {
-      // if note is empty, we don't want to do anything so remove any trace this was here
+      // if message is empty, we don't want to do anything so remove any trace this was here
       bindings.unbind('click.preventNavigation');
     }
   });
 
-  inputElement.bind('click.cancelNotePrompt', function(e) {
+  submitElement.bind('click.cancelMessagePrompt', function(e) {
     // reset everything so it won't be called again
-    inputElement.unbind('click.cancelNotePrompt');
+    submitElement.unbind('click.cancelMessagePrompt');
     bindings.unbind('click.preventNavigation');
   });
 });
 
-function noteConfirmation(textarea, tab, bindings, clickedElement) {
+function messageConfirmation(textarea, tab, bindings, clickedElement) {
 
   // unbind the event so it won't register any more clicks until they reblur the textarea again
   bindings.unbind('click.preventNavigation'); 
 
   var html = [
-    '<fieldset class="inline-form note-confirmation">', 
+    '<fieldset class="inline-form message-confirmation">', 
       '<legend class="inline-form-head">Message Confirmation</legend>', 
       '<div class="content-alerts content-warnings">', 
         '<h4 class="title">You have an message that was not saved or sent...</h4>', 
       '</div>', 
       '<p class="form-actions">', 
-        '<a class="action note-back" href="#">Please take me back!</a>', 
+        '<a class="action message-back" href="#">Please take me back!</a>', 
         '<a class="cancel-link" href="#">cancel</a>', 
       '</p>', 
     '</fieldset>'].join('');
 
-  if($('.note-confirmation').length === 0) {
+  if($('.message-confirmation').length === 0) {
     // we want to make sure only one overlay gets created
     $('body').append(html);
   }
   
-  $('.note-confirmation').one('click', 'a', function(e) {
+  $('.message-confirmation').one('click', 'a', function(e) {
     var $this = $(this);
     e.preventDefault(); // we'll stop the links in the confirmation box from doing anything normally
 
     // prompt gets removed/hidden regardless of what they select
-    $('.note-confirmation').hide().remove();
+    $('.message-confirmation').hide().remove();
 
-
-    if($this.hasClass('note-back')) { // we still have reference to the removed object
+    if($this.hasClass('message-back')) { // we still have reference to the removed object
       // return them to the previous tab (if applicable) and focus the textarea
       tab.find('a').click();
-
     } else {
       if(clickedElement) { // return the normal browsing path to whatever they were doing
         if(clickedElement.is('a')) {
