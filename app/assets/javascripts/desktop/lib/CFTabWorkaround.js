4f92ee971e00ea8af40eecd9a9c633264d07eb8d
Kyle J. Ginavan <kylejginavan@gmail.com>
2012-04-30 08:27:44 -0500

    chrome frame wa

--- app/assets/javascripts/desktop/lib/CFTabWorkaround.js
+++ app/assets/javascripts/desktop/lib/CFTabWorkaround.js
@@ -15,34 +15,46 @@
 
 // This workaround uses the jQuery framework.
 function addChromeFrameFocusWorkaround() {
-    // Only apply workaround if we are in Google Chrome Frame (window.externalHost is the indicator)
-    if (navigator.userAgent.indexOf("Chrome") != -1 && window.externalHost) {
-        $(document).ready(function() {
-            // Add the empty div to the DOM
-            $("body").prepend("<div style='width: 0; height: 0;' tabindex='1' id='ChromeFrameWorkaroundDiv'></div>");
-            // Track the element that is losing focus
-            var lastFocus = null, fsElem = null, index = null;
-            $(document).focusout(function(evt) {
-                if (evt && evt.target) {
-                    lastFocus = $(evt.target);
-                    // we will get next target rather than move back to the original selection
-                    fsElem = lastFocus.parents('body').find('a, input, textarea, select');
-                    index = fsElem.index(lastFocus);
-                    
-                    // chromeframe bug for autocomplete
-                    var i = 1;
-                    // try and find the next focusable element, because we can't focus the now hidden h4s
-                    while(!fsElem.eq(index+i).is('a, input, textarea, select')) i++;
-                    lastFocus = fsElem.eq(index+i);
-                }
-            });
-            // When focus is given to the ChromeFrameWorkaroundDiv, give it back to the previous element.
-            $("#ChromeFrameWorkaroundDiv").focusin(function() {
-                if (lastFocus) {
-                    lastFocus.focus();
-                }
-            })
-        });
-    }
+  // Only apply workaround if we are in Google Chrome Frame (window.externalHost is the indicator)
+  if (navigator.userAgent.indexOf("Chrome") != -1 && window.externalHost) {
+    $(document).ready(function() {
+      // Add the empty div to the DOM
+      $("body").prepend("<div style='width: 0; height: 0;' tabindex='1' id='ChromeFrameWorkaroundDiv'></div>");
+      // Track the element that is losing focus
+      var lastFocus = null, 
+          fsElem = null, 
+          index = null, 
+          newFocus = null,
+          h4Parent = null;
+
+      $(document).focusout(function(evt) {
+        if (evt && evt.target) {
+          lastFocus = $(evt.target);
+          // we will get next target rather than move back to the original selection
+          fsElem = lastFocus.parents('body').find('a, input, textarea, select, h4').not('[tabindex="-1"');
+          index = fsElem.index(lastFocus);
+          if(lastFocus.is('h4')) {
+            h4Parent = lastFocus.parents('.autocomplete-results');
+            fsElem = h4Parent.find('h4');
+            index = fsElem.index(lastFocus);
+          }
+          newFocus = fsElem.eq(index+1);
+        }
+      });
+      // When focus is given to the ChromeFrameWorkaroundDiv, give it back to the previous element.
+      $("#ChromeFrameWorkaroundDiv").focusin(function() {
+        if(newFocus && index > -1) {
+          newFocus.focus();
+        } else {
+          lastFocus.focus(); // we'll refocus the last focused item in case something goes wrong.
+          // That way people won't lose their mind having to move their hand to the mouse to refocus and keep moving
+        }
+        if(h4Parent) setTimeout(function() { 
+          h4Parent.show(); 
+        }, 10); // probably could drop this, but at 10ms, it's almost unnoticable
+        // basically makes it guaranteed that it'll fire after the hide event is called `ie: nextTick()`
+      })
+    });
+  }
 }
 addChromeFrameFocusWorkaround();
