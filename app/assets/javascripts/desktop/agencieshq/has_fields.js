824db773cc99673277c3870205a608b7b015d339
Geoff Hodgson <geoffroh@gmail.com>
2012-07-27 02:29:21 +1000

    Small change to has_field.js

--- app/assets/javascripts/desktop/agencieshq/has_fields.js
+++ app/assets/javascripts/desktop/agencieshq/has_fields.js
@@ -1,7 +1,9 @@
 $(document).ready(function($) {
 	
 	$(".form select#field_style").live("change", function(){
-		$(this).closest("form.form").submit();
+	  if($(this).val()=="select"){
+		  $(this).closest("form.form").submit();
+	  }
 	});
 	
 });
