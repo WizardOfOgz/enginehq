8d585671fd2bb1b166c41c7229cb8522fac2bc32
Kyle J. Ginavan <kylejginavan@gmail.com>
2012-08-08 08:41:04 -0500

    removed js for select options

--- app/assets/javascripts/desktop/agencieshq/has_fields.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function($) {
-	
-	$(".form select#field_style").live("change", function(){
-	  if($(this).val()=="select"){
-		  $(this).closest("form.form").submit();
-	  }
-	});
-	
-});
